8fa61899ceb06a1d22bf9d50a313441f32274b9b
Paul M. Christian <paulmchristian@gmail.com>
2017-05-03 19:47:28 -0500

    OSC working

---
+++ Assets/OscHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+class OscHandler : MonoBehaviour
+{
+    void Start()
+    {
+        var oscIn = GetComponent<OscIn>();
+        oscIn.Map("/bcr2000", (OscMessage msg) =>
+        {
+            Debug.Log("received message " + msg.address + " " + msg.args.First());
+            
+        });
+    }
+
+    void Update()
+    {
+        
+    }
+}
